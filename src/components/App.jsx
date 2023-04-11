c575c72795cccad4b5c66db00e1a309334f877a7
MikeBeekeeper <mike.rotar.91@gmail.com>
2023-04-11 13:49:14 +0300

    final

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,59 @@
+import React, { useState } from 'react';
+import css from '../components/app.module.css';
+import Section from './Section/Section.js';
+import Statistics from './statistics/statistics.js';
+import Notification from './notification/Notification.js';
+import FeedbackOptions from './feedbackButtons/FeedbackOptions.js';
+
 export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const state = ['good', 'neutral', 'bad'];
+
+  const onClick = ({ target: { name } }) => {
+    switch (name) {
+      case 'good':
+        setGood(prev => prev + 1);
+        break;
+      case 'neutral':
+        setNeutral(prev => prev + 1);
+        break;
+      case 'bad':
+        setBad(prev => prev + 1);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const countingTotal = () => {
+    return good + neutral + bad;
+  };
+
+  const countingPercentage = () => {
+    return (good / countingTotal()) * 100;
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={css.app}>
+      <Section title="Please leave feedback">
+        <FeedbackOptions options={state} handleClick={onClick} />
+      </Section>
+      <Section title="Statistics">
+        {countingTotal() === 0 ? (
+          <Notification message="There is no feedback" />
+        ) : (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={countingTotal()}
+            percent={Math.round(countingPercentage())}
+          />
+        )}
+      </Section>
     </div>
   );
 };
---
+++ src/components/feedbackButtons/FeedbackOptions.js
@@ -0,0 +1,28 @@
+import css from '../feedbackButtons/FeedbackOptions.module.css';
+import PropTypes from 'prop-types';
+
+const FeedbackOptions = ({ options, handleClick }) => {
+  return (
+    <ul className={css.buttonWrapper}>
+      {options.map((el, index) => (
+        <li key={index}>
+          <button
+            type="button"
+            className={css.button}
+            onClick={handleClick}
+            name={el}
+          >
+            {el}
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default FeedbackOptions;
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.array.isRequired,
+  handleClick: PropTypes.func.isRequired,
+};
